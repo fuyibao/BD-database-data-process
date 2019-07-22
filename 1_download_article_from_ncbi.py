f426574bce03c9426556f35da5722d8639f14307
fuyibao <30340232+fuyibao@users.noreply.github.com>
2019-07-22 16:31:03 +0800

    从NCBI上下载对应年份的文章
    下载后的文章格式为xml,每个xml文件中包含100篇文章的内容，需要进一步解析。

---
+++ 1_download_article_from_ncbi.py
@@ -0,0 +1,34 @@
+#-*- coding: UTF-8 -*-
+from Bio import Entrez
+from math import ceil
+import sys,os
+path="/media/EXTend2018/Fuyibao2018/NCBI/2016Fulltext_XML/"
+if not os.path.exists(path):
+    os.mkdir(path)
+Entrez.email = "jiajmeurida@163.com"
+handle = Entrez.esearch(db="pmc",retmax=100,retstart=0,term = "2017[pdat]")  #日期可以修改
+records = Entrez.read(handle)
+number = records["Count"]
+#b=records["IdList"]    #获取具体的UID
+number = int(number)
+number_circle = int(ceil(number/100))       #循环次数
+retmax_circle = 100
+retstart_circle = 0
+print("total number:")
+print(number)
+i= 0  #文件名的序号
+fileList_xml = [path+'2016_fulltext_'+str(x) + '.xml' for x in range(1,number_circle+2) ]       #创建文件的名字xml
+while (retstart_circle < number):
+    handle = Entrez.esearch(db = "pmc",retmax = 100,retstart = retstart_circle,term = "2016[pdat]")
+    records = Entrez.read(handle)
+    handle.close()
+    b = records["IdList"]     #获取具体的UID
+    handle = Entrez.efetch("pmc",id = b,rettype = 'medline',retmode = "xml")       #下载UID为b的文章
+    records = handle.read()    #把handle句柄读入records中
+    with open (fileList_xml[i],"a") as f:
+        f.write(records)
+    handle.close()
+    i += 1
+    print('txt:',i)
+    retstart_circle += 100 #迭代
+print("done")
